9f573294ff1eeb70105f972bea41df46a3027c1c
Liav Batson <batsonliav@gmail.com>
2022-08-01 20:43:36 +0300

    Fix jsonwebtoken

--- Server/app/routes/auth-routes.ts
+++ Server/app/routes/auth-routes.ts
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
 const Auth = require("../controllers/auth.ts");
 
 router.post("/register", Auth.register);
-
 router.post("/login", Auth.login);
-
 router.get("/refresh", Auth.renewToken);
 
 export = router;
