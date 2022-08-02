b860a8eb8ed76444cb3bf97c9dd7340f177898ac
Liav Batson <batsonliav@gmail.com>
2022-08-02 16:37:50 +0300

    add echo to socket_server

--- Server/src/socket_server.ts
+++ Server/src/socket_server.ts
@@ -5,9 +5,15 @@ const socketServer = (server: http.Server): Server => {
     const io = new Server(server);
     io.on('connection', (socket) => {
         console.log('User Connected');
+        // Disconnect Event
         socket.on('disconnect', () => {
             console.log('User Disconnected');
         });
+        // Echo Event
+        socket.on('echo', (msg) =>{
+            console.log("Echo Event: " + msg)
+            socket.emit('echo', msg)
+        }) 
     });
     return io
 }
